56d224ef55738e8c454586dca7c000860f62e6f6
Divya Pankaja Nanda <divyananda2009@gmail.com>
2025-03-20 21:07:38 +0530

    submission and analytics module - basic

--- backend/src/modules/submission/v1/services/ISubmissionService.ts
+++ backend/src/modules/submission/v1/services/ISubmissionService.ts
@@ -1,3 +1,5 @@
+import { ISubmission } from '../../../../common/interfaces/ISubmission.js';
+
 export interface ISubmissionService {
 	findByQuestionnaireId(questionnaireId: string): Promise<Partial<ISubmission>[]>;
 	create(submission: Partial<ISubmission>): Promise<Partial<ISubmission>>;
